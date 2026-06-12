33ce54b11582df6ffed929c07468d0446df4996b
tecanto <alu.24130561@correo.itlalaguna.edu.mx>
2026-06-12 02:08:17 -0600

    pre multiplayer

--- scripts/game/player_spawner.gd
+++ scripts/game/player_spawner.gd
@@ -3,10 +3,12 @@ extends MultiplayerSpawner
 @export var player_scene: PackedScene = load("res://prefabs/player_instance.tscn")
 @export var spawn_location: Node3D
 
-func spawn_player():
+func spawn_player(peer_id: int):
 	var player = player_scene.instantiate()
+	player.name = str(peer_id)
+	player.set_multiplayer_authority(peer_id)
 	spawn_location.add_child(player)
 
 func _ready() -> void:
 	if multiplayer.is_server():
-		spawn_player()
+		spawn_player(get_multiplayer_authority())
